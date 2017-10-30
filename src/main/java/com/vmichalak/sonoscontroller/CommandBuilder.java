d1b91c5e6fe710a5a907827ec23b4b14ade400d2
Valentin Michalak <valentinmichalak@gmail.com>
2017-10-30 13:18:19 +0100

    Add UPnP Error 714 - Illegal MIME-Type

--- src/main/java/com/vmichalak/sonoscontroller/CommandBuilder.java
+++ src/main/java/com/vmichalak/sonoscontroller/CommandBuilder.java
@@ -43,6 +43,7 @@ class CommandBuilder {
         ERROR_DESCRIPTION_MAP.put(610, "Invalid Sequence");
         ERROR_DESCRIPTION_MAP.put(611, "Invalid Control Url");
         ERROR_DESCRIPTION_MAP.put(612, "No Such Session");
+        ERROR_DESCRIPTION_MAP.put(714, "Illegal MIME-Type");
     }
 
     private static OkHttpClient httpClient;
