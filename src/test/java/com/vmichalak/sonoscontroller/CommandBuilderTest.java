e996271ac8a6d3a260d1a084b9513c963acc670e
Valentin Michalak <valentinmichalak@gmail.com>
2017-11-09 20:10:31 +0100

    #21 - Add toDIDL() method, replace metadata strings by TrackMetadata object and add tests

--- src/test/java/com/vmichalak/sonoscontroller/CommandBuilderTest.java
+++ src/test/java/com/vmichalak/sonoscontroller/CommandBuilderTest.java
@@ -1,6 +1,11 @@
 package com.vmichalak.sonoscontroller;
 
+import com.vmichalak.sonoscontroller.exception.SonosControllerException;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -22,4 +27,21 @@ public class CommandBuilderTest {
         assertEquals(expected, CommandBuilder.transport("test").put("a", escaped).getBody());
     }
 
+    @Test
+    public void otherAlreadyEscapedTest() {
+        assertEquals("<a>aa&lt;bbcc</a>", CommandBuilder.transport("test").put("a", "aa&lt;bbcc").getBody());
+        assertEquals("<a>aadze&gt;bbcc</a>", CommandBuilder.transport("test").put("a", "aadze&gt;bbcc").getBody());
+        assertEquals("<a>sssaa&quot;bbcc</a>", CommandBuilder.transport("test").put("a", "sssaa&quot;bbcc").getBody());
+        assertEquals("<a>sqsda&apos;bbqdc</a>", CommandBuilder.transport("test").put("a", "sqsda&apos;bbqdc").getBody());
+    }
+
+    @Test
+    public void unescape() throws IOException, SonosControllerException {
+        MockWebServer server = new MockWebServer();
+        server.enqueue(new MockResponse().setBody("hello &amp;quot;world&amp;quot; !"));
+        server.start(1400);
+        String s = CommandBuilder.transport("test").put("a", "a").executeOn("127.0.0.1");
+        assertEquals("hello \"world\" !", s);
+        server.shutdown();
+    }
 }
